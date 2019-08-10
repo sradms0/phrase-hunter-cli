e51610c5ad96ece7473cdde5ebd80802b0f5ad97
sradms0 <sradms0@openmailbox.org>
2019-08-10 18:54:14 -0400

    add index method

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -4,7 +4,7 @@ from phrasehunter.character import Character
 class Phrase:
     def __init__(self, phrase):
         self.phrase = [Character(c) for c in phrase]
-    
+
     @property
     def guessed(self):
         return sum( [1 if c.was_guessed else 0 for c in self] ) == len(self)
@@ -13,8 +13,12 @@ class Phrase:
         for c in self.phrase: print(f'{c} ', end='')
         print()
 
+    def index(self, char):
+        return [i for i, c in enumerate(self) if c.original == char]
+
     def __iter__(self):
         yield from self.phrase
 
     def __len__(self):
         return len(self.phrase)
+
