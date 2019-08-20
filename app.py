4086a2456c547f72b4722b70e95556cf186409dd
sradms0 <sradms0@openmailbox.org>
2019-08-20 13:22:21 -0400

    add docstring

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+'''Phrase Guessing Game
+
+This script runs a singler player game, having the player guess a random phrase selected from a file. 
+
+This script requires that a 'phrases.py' file is available within the same directory of this script. This file should contains an array of strings, but should already exist within the directory, upon cloning the reposistory
+'''
+
 from phrasehunter.game import Game
 from phrases import PHRASES
 
